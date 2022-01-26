ae21bf373ed5b7102683248af770ae7c730b2238
unknown <primaabdillah5@gmail.com>
2022-01-26 08:06:30 +0700

    modul 2 upload

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Hasbi | About</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('container')
     <h1> Halaman About </h1>
     <h3> Hasbi Prima Abdillah </h3>
     <p> 3103120106@student.smktelkom-pwt.sch.id </p>
-    <img src="images/hasbi.jpeg" alt="hasbi" width="200px">
-</body>
-</html>
+    <img src="images/{{ $gambar }}" alt="namas" width="200px">
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about', [
+        "title" => "About",
+        "nama" => "Hasbi Prima Abdillah",
+        "email" => "3103120106@student.smktelkom-pwt.sch.id",
+        "gambar" => "hasbi.jpeg"
+    ]);
 });
 
 Route::get('/gallery', function () {
-    return view('gallery');
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
 });
