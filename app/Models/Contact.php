0ea6183d9f96a6427d9e9440c8a44f3fef99c4b7
unknown <primaabdillah5@gmail.com>
2022-02-24 23:38:17 +0700

    modul_4

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    use HasFactory;
+   protected $table = 'contacts';
+   protected $primaykey = 'id';
+   protected $fillable = ['id', 'nama', 'email', 'pesan'];
 }
